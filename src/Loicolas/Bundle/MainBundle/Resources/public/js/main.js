b13ea4eb25b49d43d296036e33d20b5058fac8cc
loicolas <loicolas@mac>
2014-04-08 22:26:57 +0200

    Animation when menu opening

--- src/Loicolas/Bundle/MainBundle/Resources/public/js/main.js
+++ src/Loicolas/Bundle/MainBundle/Resources/public/js/main.js
@@ -1,6 +1,20 @@
 (function($) {
     $(document).ready(function() {
         var $window = $(window);
+        var $body   = $('body').eq(0);
+        var class_menu_open = 'menu-open';
+        
+        
         $('#content').css('min-height', $window.height()+'px');
+        
+        $('#menu-opener').on('click', function(e){
+            e.preventDefault();
+                
+            if( $body.hasClass(class_menu_open) ){
+                $body.removeClass(class_menu_open);
+            } else {
+                $body.addClass(class_menu_open);
+            }
+        });
     });
 })(jQuery);
